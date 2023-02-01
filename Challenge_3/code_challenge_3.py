b0df9ca08b650f8222903335e24fbb8722992038
aderly flores <aderfloresg@gmail.com>
2023-02-01 08:39:17 -0500

    final

---
+++ Challenge_3/code_challenge_3.py
@@ -0,0 +1,11 @@
+
+object = {“x”:{“y”:{“z”:”a”}}}
+
+key = x/y/z
+
+value = a
+
+#process:
+#1st with a function capture all the values from the key string.
+#2do capture the value of the last nested key
+
